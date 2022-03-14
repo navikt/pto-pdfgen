c19a52d89809720b5a4a39aaaba9adbbc98bdf12
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-03-14 16:09:02 +0100

    oppdater workflow - bruk ghcr.io - oppdater til actions/checkout@v2 - ta i bruk docker/login-action@v1 og docker/build-push-action@v2 - ta bort default-verider i nais.yaml

--- run_development.sh
+++ run_development.sh
@@ -5,7 +5,7 @@ PDFGEN_VERSION=d84f2a198a1b05af62b87ce994f0a23e22260bca
 
 # Legg til for debug: -e JAVA_OPTS='-Dlogback.configurationFile=logback-remote-debug.xml' \
 
-docker pull navikt/pdfgen:$PDFGEN_VERSION
+docker pull ghcr.io/navikt/pdfgen/pdfgen:$PDFGEN_VERSION
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
