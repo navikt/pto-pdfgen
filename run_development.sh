4257a0b701d3907a8856c6260033fda087ea7d74
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-03-21 16:46:12 +0100

    Oppdater til nyeste pdfgen-versjon

--- run_development.sh
+++ run_development.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 CURRENT_PATH="$(pwd)"
-PDFGEN_VERSION=d84f2a198a1b05af62b87ce994f0a23e22260bca
+PDFGEN_VERSION=1.4.3
 
 # Legg til for debug: -e JAVA_OPTS='-Dlogback.configurationFile=logback-remote-debug.xml' \
 
-docker pull ghcr.io/navikt/pdfgen/pdfgen:$PDFGEN_VERSION
+docker pull ghcr.io/navikt/pdfgen:$PDFGEN_VERSION
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
@@ -15,4 +15,4 @@ docker run \
         -e DISABLE_PDF_GET=false \
         -it \
         --rm \
-        navikt/pdfgen:$PDFGEN_VERSION
+        ghcr.io/navikt/pdfgen:$PDFGEN_VERSION
