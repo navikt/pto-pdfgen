c53d86017441e064f3de4e93c11df5a90b299314
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-03-22 11:07:39 +0100

    Fjern /resources

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM ghcr.io/navikt/pdfgen:1.4.3
 COPY templates /app/templates
 COPY fonts /app/fonts
-# COPY resources /app/resources
--- run_development.sh
+++ run_development.sh
@@ -9,7 +9,6 @@ docker pull ghcr.io/navikt/pdfgen:$PDFGEN_VERSION
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
-        -v $CURRENT_PATH/resources:/app/resources \
         -v $CURRENT_PATH/data:/app/data \
         -p 8081:8080 \
         -e DISABLE_PDF_GET=false \
